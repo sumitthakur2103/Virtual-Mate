3f70b79e3adf95698eb75dfc5adc65f0b0329e33
Sumit Thakur <tsumit0505@gmail.com>
2025-02-15 00:33:39 +0530

    initial commit

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        token: { type: String }
+    }
+)
+
+const User = mongoose.model("User",userSchema);
+
+export {User}
