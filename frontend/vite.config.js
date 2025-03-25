d7d10f00805c752d75988488c3c893f61f0e1267
Sumit Thakur <tsumit0505@gmail.com>
2025-03-25 23:41:29 +0530

    trying to solve deployment error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Ensure the base property is correctly set for deployment
   plugins: [
     react(),
     tailwindcss()
